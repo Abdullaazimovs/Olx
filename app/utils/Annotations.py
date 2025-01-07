ce8baa612c8125a6359ef70069e4de2945beed24
Abdulla <azimovabdulla6@gmail.com>
2025-01-07 15:19:40 +0500

    First run

--- app/utils/Annotations.py
+++ app/utils/Annotations.py
@@ -1,11 +1,15 @@
 from typing import Annotated
-
 from fastapi import Depends
 from sqlalchemy.orm import Session
-
 from database.db import get_db
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Form
+
+
+class EmailPasswordForm:
+    def __init__(self, email: str = Form(...), password: str = Form(...)):
+        self.email = email
+        self.password = password
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
-form_data = Annotated[OAuth2PasswordRequestForm, Depends()]
+form_data = Annotated[EmailPasswordForm, Depends()]
